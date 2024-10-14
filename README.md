f4fd3f0d615369ea1a49a0d11d0701bab2e14514
unknown <huytronglee@gmail.com>
2024-10-15 01:05:06 +0700

    Sửa lỗi Template + add Vite

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-FrontEnd_RMRBD SYSTEM
+# FrontEnd_RMRBD SYSTEM
 
 
-# React + Vite
+React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Navbar from './components/Navbar/Navbar';
-import CustomerList from './components/API Test/CustomerList';
 import './index.css';
 
 
 const App = () => {
   return (
  <div>
-    <div className="container mx-auto">
-    <CustomerList />
+  <div className="container mx-auto">
+  <Navbar/>
   </div>
   </div>
   );
